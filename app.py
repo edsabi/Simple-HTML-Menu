c1cb73d18602051d9000adbe20f828ea3a28d5ad
edsabi <38739707+edsabi@users.noreply.github.com>
2024-03-15 22:03:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, send_file
 app = Flask(__name__)
 
 # Replace 'your_html_file_path.html' with the path to your HTML file
-html_file_path = 'templates/menu_with_qr_placeholder.html'
+html_file_path = 'templates/menu.html'
 
 @app.route('/')
 def serve_menu():
